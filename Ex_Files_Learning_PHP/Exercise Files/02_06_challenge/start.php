5a633713926708f2ebdf6a7436f19fb3bf4fc36e
atimw36 <atimw36@gmail.com>
2022-03-08 09:37:05 -0500

    Fixed up control structure challenge solution

--- Ex_Files_Learning_PHP/Exercise Files/02_06_challenge/start.php
+++ Ex_Files_Learning_PHP/Exercise Files/02_06_challenge/start.php
@@ -2,18 +2,18 @@
 
 $min = 1; 
 $max = 50;
-$guess = 4; //Change this value to test!
+$guess = 5; //Change this value to test!
 // If you really want to go nuts, try this:
 //$num = rand($min, $max);
 $num = rand($min, $max);
 
-if ($guess === $num){
-	echo '<p>You are correct!</p>';
+if ($guess > $max || $guess < $min){
+	echo '<p>Your number is out of range. Please guess between 1 and 50.</p>';
 } else if ($guess > $num){
 	echo '<p>Your guess was too high!</p>';
 } else if ($guess < $num){
 	echo '<p>Your guess was too low!</p>';
-} else {
-	echo '<p>You guess was invalid!</p>';
+} else if ($guess === $num){
+	echo '<p>You are correct!</p>';
 }
-
+echo '<p>The correct number is, ' . $num . '</p>';
