34502f9375ba8e2768d409247309043ed390ebec
Tim Windhorst <tim-air@Tims-Air.home>
2022-03-20 19:58:55 -0400

    started email challenege

---
+++ Ex_Files_Learning_PHP/Exercise Files/email_challenge/index.php
@@ -0,0 +1,21 @@
+
+<form method="post" name="contact" action="process.php">
+<!--name-->
+	<label for="name">Name:</label>
+	<input type="text" id="name" placeholder="Your name"><br>
+<!--email-->
+	<label for="email">EMail:</label>
+	<input type="email" id="email" placeholder="Your email"><br>
+<!--reason-->
+	<label for="reason">Reason for contacting:</label>
+	<select>
+		<option>Question</option>
+		<option>Concern</option>
+		<option>Greeting</option>
+	</select><br>
+<!--message-->	
+	<label for="message">Message:</label>
+	<input type="text" id="message" placeholder="Your message" maxlength="120"><br>
+<!--submit-->
+  <input type="submit" value="Submit">
+</form> 
