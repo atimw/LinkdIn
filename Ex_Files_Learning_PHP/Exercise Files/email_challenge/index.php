f764c38598a7ee41cff351f247e164051bc7201b
Tim Windhorst <tim-air@Tims-Air.home>
2022-03-25 22:23:57 -0400

    working on email

--- Ex_Files_Learning_PHP/Exercise Files/email_challenge/index.php
+++ Ex_Files_Learning_PHP/Exercise Files/email_challenge/index.php
@@ -2,20 +2,20 @@
 <form method="post" name="contact" action="process.php">
 <!--name-->
 	<label for="name">Name:</label>
-	<input type="text" id="name" placeholder="Your name"><br>
+	<input type="text" name="name" placeholder="Your name"><br>
 <!--email-->
 	<label for="email">EMail:</label>
-	<input type="email" id="email" placeholder="Your email"><br>
+	<input type="email" name="email" placeholder="Your email"><br>
 <!--reason-->
 	<label for="reason">Reason for contacting:</label>
-	<select>
+	<select name="reason">
 		<option>Question</option>
 		<option>Concern</option>
 		<option>Greeting</option>
 	</select><br>
 <!--message-->	
 	<label for="message">Message:</label>
-	<input type="text" id="message" placeholder="Your message" maxlength="120"><br>
+	<input type="text" name="message" placeholder="Your message" maxlength="120"><br>
 <!--submit-->
   <input type="submit" value="Submit">
 </form> 
--- Ex_Files_Learning_PHP/Exercise Files/email_challenge/process.php
+++ Ex_Files_Learning_PHP/Exercise Files/email_challenge/process.php
@@ -1,2 +1,13 @@
 <?php
-echo "Hello";
+
+$to = 'tim@example.com';
+$subject = $_POST['reason'];
+$message = $_POST['message'];
+
+$sendMail = mail($to, $subject, $message);
+
+if (!$sendMail){
+	echo "An error occured sending your message";
+} else {
+	echo "Message sent!";
+}
